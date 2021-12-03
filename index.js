631612df6d232e3ab7ff1144d51e6e717fc256d3
Md Ridwan <72821054+mdridwwan@users.noreply.github.com>
2021-12-03 21:03:12 +0600

    last update

---
+++ index.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const admin = require("firebase-admin");
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// doctors-portal-irebase-adminsdk.json
+
+
+const serviceAccount = require('./doctors-portal-irebase-adminsdk.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufevr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri)
+
+async function verifyToken(req, res, next){
+    if(req.headers.authorization?.startsWith('Bearer ')){
+        const token = req.headers.authorization.split(' ')[1];
+
+        try{
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch{
+
+        }
+    }
+    next();
+}
+
+async function run (){
+       try {
+           await client.connect();
+          const database = client.db('doctors_protal');
+          const appointmetsCollection = database.collection('appointment');
+          const userCollection = database.collection('users');
+
+           app.get('/appointments', verifyToken, async(req, res) =>{
+               const email = req.query.email;
+               const date = new Date(req.query.date).toLocaleDateString();
+            //  console.log(date)
+               const query = {email: email, date: date}
+            //   console.log(query)
+               const cursor = appointmetsCollection.find(query);
+               const appointments = await cursor.toArray();
+               res.json(appointments);
+           }) 
+           app.get('/users/:email', async(req, res) =>{
+               const email = req.params.email;
+               const query = {email: email };
+               const user = await userCollection.findOne(query);
+               let isAdmin = false;
+               if(user?.role === 'admin'){
+                    isAdmin= true;
+               }
+               res.json({admin: isAdmin});
+           })
+
+          app.post('/appointments', async (req, res) =>{
+            const appointment = req.body;
+            const result = await appointmetsCollection.insertOne(appointment);
+            res.json(result)
+          });
+
+          app.post('/users', async (req, res) =>{
+              const user = req.body;
+              const result = await userCollection.insertOne(user);
+              res.json(result);
+             // console.log(result);
+          })
+
+        app.put('/users', async (req, res) =>{
+            const user = req.body;
+            // console.log('put', user);
+            const filter = { email: user.email };
+            // data na paile add kory dive upsert: true
+            const options = { upsert: true};
+            const updateDoc = {$set: user};
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+
+           app.put('/users/admin', verifyToken, async (req, res)=>{
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if(requester){
+                const requesterAccount = await userCollection.findOne({email: requester});
+                if(requesterAccount.role === 'admin'){
+                    console.log('put', req.decodedEmail);
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await userCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else{
+                res.status(403).json({ message: 'you do not have access to make admin'})
+            }
+     
+        })
+
+       }
+
+       finally{
+        //    await client.close();
+       }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hellow Doctors portal!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening at ${port}`)
+})
